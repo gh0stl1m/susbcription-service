aca84698c8010068b89b01fcde9aa2b77dd37ef3
Santiago Sanchez Taborda <santiago.sanchez.t@gmail.com>
2023-08-11 00:45:03 -0500

    feat: Add configuration struct with context

--- main.go
+++ main.go
@@ -1,12 +1,37 @@
 package main
 
 import (
+	"database/sql"
+	"log"
+	"os"
+	"sync"
 
-  postgres "github.com/gh0stl1m/subscription-service/drivers/postgres"
+	"github.com/alexedwards/scs/v2"
+	httpDriver "github.com/gh0stl1m/subscription-service/drivers/http"
+	postgres "github.com/gh0stl1m/subscription-service/drivers/postgres"
 )
+
+type Config struct {
+  Session *scs.SessionManager
+  DB *sql.DB
+  InfoLog *log.Logger
+  ErrorLog *log.Logger
+  Wait *sync.WaitGroup
+}
 
 func main() {
 
   db := postgres.NewConnection()
-  db.Ping()
+  sessionManager := httpDriver.CreateSession()
+  wg := sync.WaitGroup{}
+
+  app := Config{
+    Session: sessionManager,
+    DB: db,
+    InfoLog: log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime),
+    ErrorLog: log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile),
+    Wait: &wg,
+  }
+
+  app.InfoLog.Println("Server started");
 }
